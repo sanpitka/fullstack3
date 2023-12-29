217bbf3b76b2af352e42189beb382470bdabdce1
sanpitka <sanpitka@student.oulu.fi>
2023-12-29 12:15:59 +0200

    dist

--- index.js
+++ index.js
@@ -6,6 +6,7 @@ const app = express()
 app.use(express.json())
 app.use(morgan(':method :url :status :res[content-length] - :response-time ms :body'))
 app.use(cors())
+app.use(express.static('dist'))
 
 let phonebook = [
     {
