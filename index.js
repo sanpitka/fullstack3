205d51e40063b7f8fd4218d85861a9a3abdfcba4
sanpitka <sanpitka@student.oulu.fi>
2023-12-29 11:58:59 +0200

    3.9

--- index.js
+++ index.js
@@ -1,9 +1,11 @@
 const express = require('express')
 const morgan = require('morgan')
+const cors = require('cors')
 const app = express()
 
 app.use(express.json())
 app.use(morgan(':method :url :status :res[content-length] - :response-time ms :body'))
+app.use(cors())
 
 let phonebook = [
     {
@@ -96,6 +98,6 @@ let phonebook = [
     response.status(204).end()
   })  
 
-const PORT = 3001
+const PORT = process.env.PORT || 3001
 app.listen(PORT)
 console.log(`Server running on port ${PORT}`)
