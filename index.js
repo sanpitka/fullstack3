f413483958b290e0b70a5be6175859d7ef9b04b6
sanpitka <sanpitka@student.oulu.fi>
2023-12-31 14:22:28 +0200

    port

--- index.js
+++ index.js
@@ -100,5 +100,6 @@ let phonebook = [
   })  
 
 const PORT = process.env.PORT || 3001
-app.listen(PORT)
-console.log(`Server running on port ${PORT}`)
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`)
+})
