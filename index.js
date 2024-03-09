9ca86335bf3def0bf917e6a56a24bf4ee1a28c61
sanpitka <sanpitka@student.oulu.fi>
2024-03-09 21:55:25 +0200

    3.16

--- index.js
+++ index.js
@@ -17,42 +17,48 @@ app.use(morgan(':method :url :status :res[content-length] - :response-time ms :c
 
 app.get('/info', (request, response) => {
     const date = new Date()
-    Person.find({}).then(persons => {
-        response.send(`<p>Phonebook has info for ${persons.length} people</p>
-        <p>${date}</p>`)
-    })
+    Person.find({})
+        .then(persons => {
+            response.send(`<p>Phonebook has info for ${persons.length} people</p>
+            <p>${date}</p>`)
+        })
+        .catch(error => next(error))
 })
 
-app.get('/api/persons', (request, response) => {
-    Person.find({}).then(persons => {
-      response.json(persons)  
-    })
+app.get('/api/persons', (request, response, next) => {
+    Person.find({})
+        .then(persons => {
+        response.json(persons)  
+        })
+        .catch(error => next(error))
     
 })
 
 app.get('/api/persons/:id', (request, response, next) => {
     Person.findById(request.params.id)
-    .then(person => {
-        if (person) {
-            response.json(person)
-        } else {
-            response.status(404).end()
-        }
-    })
-    .catch(error => next(error))
+        .then(person => {
+            if (person) {
+                response.json(person)
+            } else {
+                response.status(404).end()
+            }
+        })
+        .catch(error => next(error))
 })
 
 const personExists = (name) => {
-    Person.find({}).then(persons => {
-        const duplicate = persons.find(person => person.name === name)
-        if (duplicate) {
-            return true
-        } else return false
-    })
+    Person.find({})
+        .then(persons => {
+            const duplicate = persons.find(person => person.name === name)
+            if (duplicate) {
+                return true
+            } else return false
+        })
+        .catch(error => next(error))
 }
 
 
-app.post('/api/persons', (request, response) => {
+app.post('/api/persons', (request, response, next) => {
     const body = request.body
     
     if (body.name === undefined) {
@@ -76,9 +82,11 @@ app.post('/api/persons', (request, response) => {
         number: body.number,
     })
 
-    person.save().then(savedPerson => {
-        response.json(savedPerson)
-    })
+    person.save()
+        .then(savedPerson => {
+            response.json(savedPerson)
+        })
+        .catch(error => next(error))
 })
 
 app.delete('/api/persons/:id', (request, response, next) => {
