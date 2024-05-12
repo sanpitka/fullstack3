6409379d962f369f3ece89a45b909b9726ee0b59
sanpitka <sanpitka@student.oulu.fi>
2024-05-13 00:10:06 +0300

    3.22

---
+++ eslint.config.mjs
@@ -0,0 +1,21 @@
+import globals from "globals";
+import pluginJs from "@eslint/js";
+
+export default [
+  { files: ["**/*.js"], languageOptions: { sourceType: "commonjs", } },
+  { languageOptions: { globals: globals.browser } },
+  pluginJs.configs.recommended,
+  {
+    "env": {
+      "browser": true,
+      "node": true
+    },
+    "rules": {
+      'eqeqeq': 'error',
+      'no-trailing-spaces': 'error',
+      'object-curly-spacing': ['error', 'always'],
+      'arrow-spacing': ['error', { 'before': true, 'after': true }],
+      'no-console': 0,
+    }
+  }
+];
--- models/person.js
+++ models/person.js
@@ -1,4 +1,5 @@
 const mongoose = require('mongoose')
+const process = require('process');
 
 mongoose.set('strictQuery', false)
 
@@ -6,7 +7,7 @@ const url = process.env.MONGODB_URI
 
 console.log('connecting to MongoDB')
 mongoose.connect(url)
-  .then(result => {
+  .then(() => {
     console.log('connected to MongoDB')
   })
   .catch((error) => {
