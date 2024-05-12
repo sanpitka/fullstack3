085c85dde99ca0528e58b9b8044fdf6d4054f476
sanpitka <sanpitka@student.oulu.fi>
2024-05-12 23:32:39 +0300

    3.21

--- index.js
+++ index.js
@@ -102,7 +102,9 @@ const unknownEndpoint = (request, response) => {
 app.use(unknownEndpoint)
 
 const errorHandler = (error, request, response, next) => {
+
     console.error(error.message)
+    
     if (error.name === 'CastError') {
       return response.status(400).send({ error: 'malformatted id' })
     } else if (error.name === 'ValidationError') {
